a3046c1da058c385fbb832bcf9b2d628dc3c7794
alim <149599272+Lim018@users.noreply.github.com>
2026-04-01 23:40:30 +0700

    test swap

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,4 @@
 //   process.env.NEXT_PUBLIC_GAS_BASE_URL ?? "";
 
 
-export const BASE_URL = "https://script.google.com/macros/s/AKfycbyGpPQUZJLmZ5fJDTPxoM8MawqEb5QXEXjSmJqtAf333c_3Fc_vFfRLSPzVxljBNvtV/exec";
+export const BASE_URL = "https://script.google.com/macros/s/AKfycbxgSv71JmAE9pAEx93mikmyO1ML1p2b9zTSPqPKZy1807I-ve5FsMgSdTAfpDFpvwNG/exec";
